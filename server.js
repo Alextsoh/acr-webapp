2e3706f5bb296e7ad22bb6fbd3945f0e5ebb6459
Alextsoh <56512088+Alextsoh@users.noreply.github.com>
2025-10-06 15:18:40 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-app.get('/', (_, res) => res.send('Hello from ACR → Web App!!!'));
+app.get('/', (_, res) => res.send('Hello from ACR → Web beta!!!'));
 app.get('/healthz', (_, res) => res.send('ok'));
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
